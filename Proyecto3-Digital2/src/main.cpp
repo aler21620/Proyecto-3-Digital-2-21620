9d72624d503579e3cc52bbf72e04bc9f8130a9e1
aler21620 <rod21620@uvg.edu.gt>
2023-11-11 15:31:29 -0600

    Update main.cpp
    Cambio de programación del Neopíxel, debido a que la anterior interrumpía la lectura de la temperatura

--- Proyecto3-Digital2/src/main.cpp
+++ Proyecto3-Digital2/src/main.cpp
@@ -8,30 +8,25 @@
 // Librerías
 //*****************************************************************************
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h> 
-#include <Temperature_LM75_Derived.h> 
+#include <Adafruit_NeoPixel.h>
+#include <Temperature_LM75_Derived.h>
 #include <Wire.h>
 
 //*****************************************************************************
 //  Definición de pines
 //*****************************************************************************
-#define RX_2 16 //Para comunicación serial con TIVA
-#define TX_2 17 //Para comunicación serial con TIVA
-#define PIN  23 //Para la conexión del Neopíxel
-#define NUMPIXELS  24 //Número de píxeles del neopíxel 
+#define RX_2 16       // Para comunicación serial con TIVA
+#define TX_2 17       // Para comunicación serial con TIVA
+#define CIRCLE_PIN 25 // Para la conexión del Neopíxel
+#define NUM_CIRCLE_LEDS 24 //Número de pines del Neopíxel 
+#define BRIGHT 100   // Brillo del Neopíxel
 
-Adafruit_NeoPixel pixels(NUMPIXELS,PIN, NEO_GRB); //Funciones de la librería 
+Adafruit_NeoPixel circle(NUM_CIRCLE_LEDS, CIRCLE_PIN, NEO_GRB + NEO_KHZ800);
 
 //*****************************************************************************
 // Prototipos de función
 //*****************************************************************************
 void temperatura(void);
-void setPixel(int Pixel, byte red, byte green, byte blue);
-void Strobe(byte red, byte green, byte blue, int StrobeCount, int FlashDelay, int EndPause);
-void showStrip(void);
-void colorWipe(byte red, byte green, byte blue, int SpeedDelay);
-void setAll(byte red, byte green, byte blue);
-void BouncingColoredBalls(int BallCount, byte colors[][3]);
 
 //*****************************************************************************
 // Variables Globales
@@ -44,180 +39,96 @@ Generic_LM75 temperature;
 // Configuración
 //*****************************************************************************
 void setup() {
-  //Comunicación UART0 con la computadora Serial (0)
+  // Comunicación UART0 con la computadora Serial (0)
   Serial.begin(115200);
   Wire.begin();
   Serial.println("Se configuró Serial 0");
-  Serial2.begin(115200, SERIAL_8N1, RX_2, TX_2); //Establecer comunicación serial con TIVA
+  Serial2.begin(115200, SERIAL_8N1, RX_2, TX_2); // Establecer comunicación serial con TIVA
 
-  pixels.begin(); 
-  pixels.clear();
-  pixels.setBrightness(255);
-
-  pixels.setPixelColor(0, pixels.Color(255,100,150));
-  pixels.show();
-  
+  circle.begin();
+  circle.clear();
+  circle.setBrightness(BRIGHT);
 }
 
 //*****************************************************************************
 // Loop
 //*****************************************************************************
-void loop() {
+void loop()
+{
+  uint8_t rojo = 255;
+  uint8_t verde = 0;
+  uint8_t azul = 0;
+
+  for (int i = 0; i < NUM_CIRCLE_LEDS; i++)
+  {
+    circle.setPixelColor(i, rojo, verde, azul);
+    circle.show();
+    delay(40);
+    azul = azul + 12;
+    rojo = rojo - 12;
+  }
+
+  rojo = 0;
+  verde = 255;
+  azul = 0;
+  circle.clear();
+
+  for (int i = NUM_CIRCLE_LEDS - 1; i >= 0; i--)
+  {
+    circle.setPixelColor(i, rojo, verde, azul);
+    circle.show();
+    delay(50);
+  }
+
+  // And again
+  verde = 0;
+  azul = 255;
+  circle.clear();
+
+  for (int i = 0; i < NUM_CIRCLE_LEDS; i++)
+  {
+    circle.setPixelColor(i, rojo, verde, azul);
+    circle.show();
+    delay(30);
+    verde += 8;
+  }
+
+  // Clear down the circle LEDs and start again
+  circle.clear();
+  circle.show();
+
   temperatura();
   // Recibir datos de la TIVA C para colocar en la LCD
-  if (Serial2.available()) {
+  if (Serial2.available())
+  {
     senal = Serial2.read();
   }
 
-  if(senal == '1') {
-    temp = temperature.readTemperatureC();  // Asignar la temperatura leída a 'temp'
+  if (senal == '1')
+  {
+    temp = temperature.readTemperatureC(); // Asignar la temperatura leída a 'temp'
     Serial2.println(temp);
     Serial.print("Dato enviado a TIVA C: ");
     Serial.print(temp);
     Serial.print("°C 🌡️ \n");
-    senal = 0; 
+    senal = 0;
   }
 
-  if(senal == '2') {
-    //Agregar función de neopíxel 
-    senal = 0; 
+  if (senal == '2')
+  {
+    // Agregar función de neopíxel
+    senal = 0;
   }
-  
-  /* //Funciones para el neopíxel 
-  byte colors[3][3] = { {0xff, 0,0}, 
-                        {0xff, 0xff, 0xff}, 
-                        {0   , 0   , 0xff} };
-
-  BouncingColoredBalls(3, colors);
-  colorWipe(0x00,0xff,0x00, 50);
-  colorWipe(0x00,0x00,0x00, 50);
-
-  /*delay(100); 
-  
-  pixels.setBrightness(255);
-
-  for(int i =0; i < 16; i++){
-    pixels.setPixelColor(i, pixels.Color(random(255),random(255),random(255)));
-    pixels.show();
-    delay(100); // this speeds up the simulation
-  
-  }
-  for(int i =16; i < 32; i++){
-    pixels.setPixelColor(i, pixels.Color((255),(255),(255)));
-    pixels.show();
-    delay(100); // this speeds up the simulation
-  
-  }
-  pixels.clear();
-  pixels.show();
-*/
 }
 
 //*****************************************************************************
 // Funciones
 //*****************************************************************************
-void temperatura(void) {
+void temperatura(void)
+{
   temp = temperature.readTemperatureC();
   Serial.print("Temperature = ");
   Serial.print(temp);
   Serial.println(" C");
   delay(250);
-}
-
-void Strobe(byte red, byte green, byte blue, int StrobeCount, int FlashDelay, int EndPause){
-  for(int j = 0; j < StrobeCount; j++) {
-    setAll(red,green,blue);
-    showStrip();
-    delay(FlashDelay);
-    setAll(0,0,0);
-    showStrip();
-    delay(FlashDelay);
-  }
- 
- delay(EndPause);
-}
-
-void showStrip() {
- #ifdef ADAFRUIT_NEOPIXEL_H 
-   // NeoPixel
-   pixels.show();
- #endif
- #ifndef ADAFRUIT_NEOPIXEL_H
-   // FastLED
-   FastLED.show();
- #endif
-}
-void colorWipe(byte red, byte green, byte blue, int SpeedDelay) {
-  for(uint16_t i=0; i<NUMPIXELS; i++) {
-      setPixel(i, red, green, blue);
-      showStrip();
-      delay(SpeedDelay);
-  }
-}
-
-void setPixel(int Pixel, byte red, byte green, byte blue) {
- #ifdef ADAFRUIT_NEOPIXEL_H 
-   // NeoPixel
-   pixels.setPixelColor(Pixel, pixels.Color(red, green, blue));
- #endif
- #ifndef ADAFRUIT_NEOPIXEL_H 
-   // FastLED
-   leds[Pixel].r = red;
-   leds[Pixel].g = green;
-   leds[Pixel].b = blue;
- #endif
-}
-
-void setAll(byte red, byte green, byte blue) {
-  for(int i = 0; i < NUMPIXELS; i++ ) {
-    setPixel(i, red, green, blue); 
-  }
-  showStrip();
-}
-
-void BouncingColoredBalls(int BallCount, byte colors[][3]) {
-  float Gravity = -9.81;
-  int StartHeight = 1;
-  
-  float Height[BallCount];
-  float ImpactVelocityStart = sqrt( -2 * Gravity * StartHeight );
-  float ImpactVelocity[BallCount];
-  float TimeSinceLastBounce[BallCount];
-  int   Position[BallCount];
-  long  ClockTimeSinceLastBounce[BallCount];
-  float Dampening[BallCount];
-  
-  for (int i = 0 ; i < BallCount ; i++) {   
-    ClockTimeSinceLastBounce[i] = millis();
-    Height[i] = StartHeight;
-    Position[i] = 0; 
-    ImpactVelocity[i] = ImpactVelocityStart;
-    TimeSinceLastBounce[i] = 0;
-    Dampening[i] = 0.90 - float(i)/pow(BallCount,2); 
-  }
-
-  while (true) {
-    for (int i = 0 ; i < BallCount ; i++) {
-      TimeSinceLastBounce[i] =  millis() - ClockTimeSinceLastBounce[i];
-      Height[i] = 0.5 * Gravity * pow( TimeSinceLastBounce[i]/1000 , 2.0 ) + ImpactVelocity[i] * TimeSinceLastBounce[i]/1000;
-  
-      if ( Height[i] < 0 ) {                      
-        Height[i] = 0;
-        ImpactVelocity[i] = Dampening[i] * ImpactVelocity[i];
-        ClockTimeSinceLastBounce[i] = millis();
-  
-        if ( ImpactVelocity[i] < 0.01 ) {
-          ImpactVelocity[i] = ImpactVelocityStart;
-        }
-      }
-      Position[i] = round( Height[i] * (NUMPIXELS - 1) / StartHeight);
-    }
-  
-    for (int i = 0 ; i < BallCount ; i++) {
-      setPixel(Position[i],colors[i][0],colors[i][1],colors[i][2]);
-    }
-    
-    showStrip();
-    setAll(0,0,0);
-  }
 }
